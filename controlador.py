ad13b4fd0d1852e4f89c463b2d0caf991e93950e
Lizzy <ceronsamperiolizeth@gmail.com>
2026-04-21 20:06:43 -0600

    Version 0.3 - Cambio en el controlador

--- controlador.py
+++ controlador.py
@@ -11,19 +11,16 @@ class Controlador:
         while repetir:
             self.vista.encabezado()
             
-            # 1. Pedir datos a la vista
             eta, w_inicial = self.vista.pedir_condiciones_iniciales()
             
-            # 2. Configurar el modelo
             self.modelo.parametros(eta, w_inicial)
             
-            # 3. Entrenar (El controlador espera a que el modelo termine)
             ha_convergido, iteracion, w_final = self.modelo.entrenar()
             
-            # 4. Mostrar resultados en texto
             self.vista.mostrar_resultados(ha_convergido, iteracion, w_final)
+
+            self.vista.mostrar_historial(self.modelo.historial)
             
-            # 5. Mostrar gráfica 3D
             self.vista.dibujar_grafica_3d(
                 self.modelo.X, 
                 self.modelo.C_objetivo, 
@@ -32,5 +29,4 @@ class Controlador:
                 iteracion
             )
             
-            # 6. ¿Repetir?
             repetir = self.vista.preguntar_repetir()
